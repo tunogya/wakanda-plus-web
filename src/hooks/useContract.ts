4a6ae3df09cef3ba75ac2eaf5bf4dc751357eb6f
tunogya <tunogya@qq.com>
2022-04-05 15:00:54 +0800

    update

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,17 +1,12 @@
 import { Contract } from '@ethersproject/contracts'
-import ARGENT_WALLET_DETECTOR_ABI from '../abis/argent-wallet-detector.json'
 import ERC20_ABI from '../abis/erc20.json'
 import ERC20_BYTES32_ABI from '../abis/erc20_bytes32.json'
-import WETH_ABI from '../abis/weth.json'
 import EIP_2612 from '../abis/eip_2612.json'
+import CAPANDTRADE_ABI from '../abis/cap-and-trade.json'
 
-import {
-  ARGENT_WALLET_DETECTOR_ADDRESS,
-} from '../constants/addresses'
 import { useMemo } from 'react'
 import { getContract } from '../utils'
-import { Erc20, ArgentWalletDetector, Weth } from '../abis/types'
-import { WETH9_EXTENDED } from '../constants/tokens'
+import { Erc20 } from '../abis/types'
 import { useActiveWeb3React } from './web3'
 
 // returns null on errors
@@ -41,19 +36,14 @@ export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: b
   return useContract<Erc20>(tokenAddress, ERC20_ABI, withSignerIfPossible)
 }
 
-export function useWETHContract(withSignerIfPossible?: boolean) {
-  const { chainId } = useActiveWeb3React()
-  return useContract<Weth>(chainId ? WETH9_EXTENDED[chainId]?.address : undefined, WETH_ABI, withSignerIfPossible)
-}
-
-export function useArgentWalletDetectorContract() {
-  return useContract<ArgentWalletDetector>(ARGENT_WALLET_DETECTOR_ADDRESS, ARGENT_WALLET_DETECTOR_ABI, false)
-}
-
 export function useBytes32TokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
   return useContract(tokenAddress, ERC20_BYTES32_ABI, withSignerIfPossible)
 }
 
 export function useEIP2612Contract(tokenAddress?: string): Contract | null {
   return useContract(tokenAddress, EIP_2612, false)
+}
+
+export function useCapAndTradeContract(contractAddress?: string, withSignerIfPossible?: boolean) {
+  return useContract(contractAddress, CAPANDTRADE_ABI, withSignerIfPossible)
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,55 @@
-import React from "react"
+import React, {useCallback, useEffect, useState} from "react"
 import {Button, Spacer, Stack, Text} from "@chakra-ui/react"
 import Web3ReactManager from "../components/Web3ReactManager"
 import WalletModal from "../components/Web3Status";
+import {useCapAndTradeContract, useTokenContract} from "../hooks/useContract";
+import {CAPANDTRADE_ADDRESS, WCO2_ADDRESS} from "../constants/addresses";
+import {useActiveWeb3React} from "../hooks/web3";
+import {formatNumber, parseToBigNumber} from "../utils/bigNumberUtil";
+import useInterval from "@use-it/interval";
+import {t, Trans} from "@lingui/macro"
 
 function App() {
+  const {chainId, account} = useActiveWeb3React()
+  const WCO2 = useTokenContract(WCO2_ADDRESS[chainId ?? 1])
+  const [balance, setBalance] = useState(0)
+  const CapAndTrade = useCapAndTradeContract(CAPANDTRADE_ADDRESS[chainId ?? 1])
+  const [year, setYear] = useState(0)
+  const [cap, setCap] = useState(0)
+  const [myAllowance, setMyAllowance] = useState(0)
+  
+  const asyncFetch = useCallback(async () => {
+    if (account && WCO2) {
+      const b = await WCO2.balanceOf(account)
+      if (b) {
+        setBalance(parseToBigNumber(b).shiftedBy(-18).toNumber())
+      }
+    }
+    if (account && CapAndTrade){
+      const timestamp = (Date.now()/1000).toFixed(0)
+      const y = await CapAndTrade.yearOf(timestamp)
+      setYear(parseToBigNumber(y).toNumber())
+      const c = await CapAndTrade.capOf(timestamp)
+      setCap(parseToBigNumber(c).shiftedBy(-18).toNumber())
+      const a = await CapAndTrade.allowances(account, y)
+      setMyAllowance(parseToBigNumber(a).shiftedBy(-18).toNumber())
+    }
+  }, [account, WCO2, CapAndTrade])
+  
+  useEffect(() => {
+    asyncFetch()
+  }, [asyncFetch])
+  
+  useInterval(()=>{
+    asyncFetch()
+  }, 10000)
+  
   return (
     <Web3ReactManager>
       <Stack h={"100vh"} bg={"#F1F7FA"} direction={"row"} justifyContent={"center"}>
         <Stack w={'400px'} bg={"white"} h={'full'} p={'24px'} spacing={'24px'}>
           <Text fontSize={16} fontWeight={"600"}>Wakanda+</Text>
-          <WalletModal />
+          <WalletModal/>
           <Stack bg={'#30848A'} w={"full"} p={8} borderRadius={24} h={'440px'}>
             <Stack direction={"row"} color={'white'} alignItems={"center"}>
               <Text fontSize={24} fontWeight={600}>WCO2</Text>
@@ -17,11 +57,20 @@ function App() {
               {/*<Text fontSize={16}>0x3820...1234</Text>*/}
             </Stack>
             <Spacer/>
-            <Text fontSize={28} fontWeight={600} color={"white"}>10 tCO2e</Text>
+            <Text fontSize={28} fontWeight={600} color={"white"}>{formatNumber(balance, 2)} tCO2e</Text>
           </Stack>
-          <Button h={'80px'} borderRadius={24} fontSize={18}>
-            Claim Carbon Credit of 2022
-          </Button>
+          {
+            myAllowance < cap && (
+              <Button h={'80px'} borderRadius={24}>
+                <Stack w={"full"}>
+                  <Text>
+                    <Trans>Claim Carbon Credit</Trans>
+                  </Text>
+                  <Text fontSize={12} color={"gray"}>{t`Allowance of ${year}: ${formatNumber(cap, 2)} tCO2e`}</Text>
+                </Stack>
+              </Button>
+            )
+          }
         </Stack>
       </Stack>
     </Web3ReactManager>
