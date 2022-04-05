6e931b403ab143782f8d8dc8c55ca221eacd170a
tunogya <tunogya@qq.com>
2022-04-05 15:34:00 +0800

    fixed

--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -84,10 +84,9 @@ export const WalletModal = () => {
   }, [setWalletView, active, error, connector, isOpen, activePrevious, connectorPrevious])
 
   const tryActivation = async (connector: AbstractConnector | undefined) => {
-    let name = ""
     Object.keys(SUPPORTED_WALLETS).map(key => {
       if (connector === SUPPORTED_WALLETS[key].connector) {
-        return (name = SUPPORTED_WALLETS[key].name)
+        return (SUPPORTED_WALLETS[key].name === '')
       }
       return true
     })
