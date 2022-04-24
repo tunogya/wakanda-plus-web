37f2d33feae52275dc2adf56efdb0c39873cf0b4
tunogya <tunogya@qq.com>
2022-04-24 20:26:21 +0800

    update address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,10 +4,10 @@ type AddressMap = { [chainId: number]: string }
 
 export const WCO2_ADDRESS: AddressMap = {
   [SupportedChainId.RINKEBY]: "",
-  [SupportedChainId.MUMBAI]: "0x50fE6696f260fC815DC3C602B71fe6C991324468",
+  [SupportedChainId.MUMBAI]: "0x87DB3d434f7321B7Eeb894B0f2aFc7775807374B",
 }
 
 export const REWARDS_ADDRESS: AddressMap = {
   [SupportedChainId.RINKEBY]: "",
-  [SupportedChainId.MUMBAI]: "0x12f5721E09Db3f14aAA8876d50f58f5EA17D9780",
+  [SupportedChainId.MUMBAI]: "0x4009Ff411E6F262D77a6860B26A56538A6744032",
 }
