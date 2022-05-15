7eb106bcedda23085d55a676432943e8e041fb5c
tunogya <tunogya@qq.com>
2022-05-15 20:13:55 +0800

    update

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -3,11 +3,9 @@ import { SupportedChainId } from "./chains"
 type AddressMap = { [chainId: number]: string }
 
 export const WCO2_ADDRESS: AddressMap = {
-  [SupportedChainId.RINKEBY]: "0x7Fd8e00bba10685794326917e3f1c0B4db2D93F7",
   [SupportedChainId.MUMBAI]: "0x87DB3d434f7321B7Eeb894B0f2aFc7775807374B",
 }
 
 export const REWARDS_ADDRESS: AddressMap = {
-  [SupportedChainId.RINKEBY]: "",
   [SupportedChainId.MUMBAI]: "0x4009Ff411E6F262D77a6860B26A56538A6744032",
 }
