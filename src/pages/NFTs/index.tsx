73f67b31e58b90da9d5d1da730e4e82d73eb5874
tunogya <tunogya@qq.com>
2022-05-15 10:08:41 +0800

    update

--- src/pages/NFTs/index.tsx
+++ src/pages/NFTs/index.tsx
@@ -9,7 +9,7 @@ const NFTs = () => {
         <Text fontWeight={'semibold'} fontSize={'2xl'}>Wakanda NFTs</Text>
         <Badge color={'black'} fontSize={'xs'} variant={'outline'} borderRadius={'full'} px={2}>Polygon</Badge>
       </VStack>
-      <Stack bg={"white"} h={'full'} p={3} borderTopStartRadius={24} spacing={3}>
+      <Stack bg={"white"} h={'full'} p={3} borderTopRadius={24} spacing={3}>
         <Stack bg={'#F0F0F0'} minH={40} borderRadius={12} p={5}>
           <Text fontWeight={"semibold"}>
             Wakanda Pass
