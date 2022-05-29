de3c859efd5e1a2cd11dbf7070765673f323684a
tunogya <tunogya@qq.com>
2022-05-29 10:45:57 +0800

    update

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -26,8 +26,8 @@ const WrappedExplore = () => {
     <>
       <Stack position={'fixed'} w={'full'} px={3} pt={'env(safe-area-inset-top)'} bg={"white"} maxW={'container.lg'}>
         <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} w={'full'} h={'48px'} pt={2}>
-          <Text fontWeight={'semibold'} fontSize={'lg'} w={'200px'}>Explore</Text>
-          { isLargerThan1000 && (
+          <Text fontWeight={'semibold'} fontSize={'lg'} w={'200px'}>{isLargerThan1000 ? 'Wakanda+' : 'Explore'}</Text>
+          {isLargerThan1000 && (
             <Stack alignItems={"center"} justifyContent={"center"}>
               <HStack h={'36px'} bg={"#F0F0F0"} px={10} borderRadius={'12px'} spacing={10}>
                 {menuList.map((item) => (
@@ -44,13 +44,13 @@ const WrappedExplore = () => {
                 ))}
               </HStack>
             </Stack>
-          ) }
-          { isLargerThan1000 && (
+          )}
+          {isLargerThan1000 && (
             <HStack w={'200px'} justifyContent={"end"}>
-              <NetworkCard />
-              <WalletAvatar />
+              <NetworkCard/>
+              <WalletAvatar/>
             </HStack>
-          ) }
+          )}
         </Stack>
       </Stack>
       <ControlBar/>
