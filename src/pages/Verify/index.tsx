7e471aa2f855be4741bc86bdd752c327a888a41d
tunogya <tunogya@qq.com>
2022-06-28 16:10:43 +0800

    add verifyMessage

--- src/pages/Verify/index.tsx
+++ src/pages/Verify/index.tsx
@@ -1,22 +1,32 @@
-import {Button, HStack, Stack, Text} from "@chakra-ui/react"
-import {WalletModal} from "../../components/Web3Status"
+import {Button, Stack, Text} from "@chakra-ui/react"
 import {useActiveWeb3React} from "../../hooks/web3";
+import { ethers } from "ethers";
 
 const Verify = () => {
-
-  const { library } = useActiveWeb3React()
+  const { library, account } = useActiveWeb3React()
 
   const handleSignature = async () => {
-    const res = await library?.getSigner().signMessage('Hello')
-    console.log(res)
+    const signature = await library?.getSigner().signMessage('Hello')
+    if (signature) {
+      const r = signature.slice(0, 66)
+      const s = '0x' + signature.slice(66, 130)
+      const v =  parseInt('0x' + signature.slice(130, 132), 16)
+      const res = ethers.utils.verifyMessage('Hello', {r: r, s: s, v: v})
+      console.log(res)
+    }
   }
 
   return (
     <Stack>
       <Text>Verify</Text>
-      <Button onClick={handleSignature}>
+      <Button
+        disabled={!account}
+        onClick={handleSignature}
+      >
         Signature
       </Button>
+
+
     </Stack>
   )
 }
