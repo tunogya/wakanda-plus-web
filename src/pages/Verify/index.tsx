c41a1632db042c1ea1add785ce9e16a406de7a0e
tunogya <tunogya@qq.com>
2022-06-29 13:46:40 +0800

    update

--- src/pages/Verify/index.tsx
+++ src/pages/Verify/index.tsx
@@ -2,14 +2,32 @@ import {Button, Center, Code, Stack, Text} from "@chakra-ui/react"
 import { useActiveWeb3React } from "../../hooks/web3"
 import { ethers } from "ethers"
 import {useSearchParams} from "react-router-dom";
+import {useCallback, useEffect, useState} from "react";
 
 const Verify = () => {
   const { library, account } = useActiveWeb3React()
+  const [payload, setPayload] = useState({
+    guild: null,
+    member: null
+  })
   const [params] = useSearchParams()
-  const guild = params.get('guild')
-  const member = params.get('member')
+  const state = params.get('state')
 
-  console.log(guild, member)
+  const fetchPayload = useCallback(async () => {
+    try {
+      const q = await fetch(`https://api.wakanda-labs.com/discord?state=${state}`)
+      const res = await q.json()
+      if (res) {
+        setPayload(res)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }, [state])
+
+  useEffect(() => {
+    fetchPayload()
+  }, [fetchPayload])
 
   const handleSignature = async () => {
     const signature = await library?.getSigner().signMessage("Hello")
@@ -28,7 +46,7 @@ const Verify = () => {
       <Stack alignItems={"center"} w={'container.sm'} spacing={4}>
         <Text fontWeight={'bold'} fontSize={'xl'}>Please sign the message below</Text>
         <Code p={4} borderRadius={'12px'} minW={'420px'} h={'160px'} colorScheme={'pink'} variant={"outline"}>
-          Guild: {guild} Member: {member}
+          Guild: {payload.guild ?? '...'} Member: {payload.member ?? '...'}
         </Code>
         <Text fontSize={'sm'} fontWeight={'semibold'}>Never share your seed phrase or private key!</Text>
         <Button disabled={!account} onClick={handleSignature}>
